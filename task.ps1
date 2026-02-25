56165ca2a5dca0f46450d2d877b771d2a4b7e6ae
AmrDab <139714613+AmrDab@users.noreply.github.com>
2026-02-24 19:19:21 -0800

    cleanup: remove dev artifacts (promo-image.html, prompt.txt, task.ps1)

--- task.ps1
+++
@@ -1,2 +0,0 @@
-param([string]$t = "Press the Windows key")
-Invoke-WebRequest -Uri "http://127.0.0.1:3847/task" -Method POST -ContentType "application/json" -Body ([System.Text.Encoding]::UTF8.GetBytes((ConvertTo-Json @{task=$t}))) | Select-Object -ExpandProperty Content
